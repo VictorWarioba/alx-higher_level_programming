0cf87b339c7d90d1e47ce09b466d08a59dc174ae
VictorWarioba <victorpython46@gmail.com>
2022-06-02 00:48:39 +0300

    alphabet soup

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
-for i in range(56, 250):
-    if chr(i) != 'q' and chr(i) != 'e':
-        print(chr(i), end="")
+for i in range(97, 123):
+    if chr(i) == 'q' or chr(i) == 'e':
+        continue
+    print("{}".format(chr(i)), end="")
